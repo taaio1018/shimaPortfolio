f72835a3e2de4e73476ff0b8e3f1f0b36f986744
Takahiro Shimada <ta.a.i.o.1018@gmail.com>
2022-02-17 16:35:32 +0900

    Finished Gatsby Tutorial Part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
-    siteMetadata: {
-      title: `MyPortfolio`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          name: `blog`,
-          path: `${__dirname}/blog`,
-        }
+  siteMetadata: {
+    title: `MyPortfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
-      "gatsby-plugin-mdx",
-    ]
-}
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+};
